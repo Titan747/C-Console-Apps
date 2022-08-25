979afa04bc290158ff6d391c3140b481b24db617
Titan Stark <add.sivakumar@gmail.com>
2022-08-25 13:06:28 +0530

    C Sharp Console Apps

---
+++ LoggingMechanism/LoggingMechanism/Program.cs
@@ -0,0 +1,33 @@
+﻿class LoggingMechanism
+{
+    public static void Main()
+    {
+        using (StreamWriter w = File.AppendText("C:\\Users\\addsi\\source\\repos\\LoggingMechanism\\LoggingMechanism\\bin\\Log\\log.txt"))
+        {
+            Log("Logging Process 1 Finished", w);
+            Log("Logging Process 2 Finished", w);
+            w.Close();
+        }
+        using (StreamReader r = File.OpenText("C:\\Users\\addsi\\source\\repos\\LoggingMechanism\\LoggingMechanism\\bin\\Log\\log.txt"))
+        {
+            DumpLog(r);
+        }
+    }
+
+    public static void Log(string logMessage, TextWriter w)
+    {
+        w.WriteLine("{0} {1}", DateTime.Now.ToLongTimeString(), DateTime.Now.ToLongDateString());
+        w.WriteLine("  :{0}", logMessage);
+        w.Flush();
+    }
+
+    public static void DumpLog(StreamReader r)
+    {
+        string line;
+        while ((line = r.ReadLine()) != null)
+        {
+            Console.WriteLine(line);
+        }
+        r.Close();
+    }
+}
---
+++ RemovingDuplicates/RemovingDuplicates/Program.cs
@@ -0,0 +1,40 @@
+﻿using Spire.Xls;
+using System.Linq;
+
+namespace RemoveDuplicateRows
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Create a Workbook instance
+            Workbook workbook = new Workbook();
+            //Load the Excel file
+            workbook.LoadFromFile("C:\\Users\\addsi\\source\\Test.xlsx");
+
+            //Get the first worksheet
+            Worksheet sheet = workbook.Worksheets[0];
+
+            //Specify the range that you want to remove duplicate records from.
+            var range = sheet.Range["A1:A" + sheet.LastRow];
+            //Get the duplicated row numbers  
+            var duplicatedRows = range.Rows
+                   .GroupBy(x => x.Columns[0].DisplayedText)
+                   .Where(x => x.Count() > 1)
+                   .SelectMany(x => x.Skip(1))
+                   .Select(x => x.Columns[0].Row)
+                   .ToList();
+
+            //Remove the duplicate rows & blank rows if any           
+            for (int i = 0; i < duplicatedRows.Count; i++)
+            {
+                sheet.DeleteRow(duplicatedRows[i] - i);
+            }
+
+            //Save the result file
+            Console.WriteLine("\n");
+            Console.WriteLine("...Duplicates Removed Successfully...");
+            workbook.SaveToFile("C:\\Users\\addsi\\source\\Output.xlsx", ExcelVersion.Version2013);
+        }
+    }
+}
